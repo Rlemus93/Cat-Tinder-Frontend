8376e1274799336bce71ea8477f12cee34a00f60
smorgannicole <morgann.smithh@yahoo.com>
2024-04-02 13:17:40 -0700

    💄 created page files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Home from './pages/Home'
+import CatEdit from './pages/CatEdit'
+import CatIndex from './pages/CatIndex'
+import CatNew from './pages/CatNew'
+import CatShow from './pages/CatShow'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
