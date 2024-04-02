540b532ef73f5444f500a9ba26b427a98cae300b
smorgannicole <morgann.smithh@yahoo.com>
2024-04-02 13:32:02 -0700

    🌱 installed react-router-dom and created page files

--- src/App.js
+++ src/App.js
@@ -7,11 +7,20 @@ import CatIndex from './pages/CatIndex'
 import CatNew from './pages/CatNew'
 import CatShow from './pages/CatShow'
 import NotFound from './pages/NotFound'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="cat-edit" element={<CatEdit />} />
+        <Route path="cat-index" element={<CatIndex />} />
+        <Route path="cat-new" element={<CatNew />} />
+        <Route path="cat-show" element={<CatShow />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
     </>
   )
