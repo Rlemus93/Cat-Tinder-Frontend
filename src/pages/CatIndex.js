1b0f1ece81ccc3bd1a6eaabafbdc18b3a3298495
Ryan <rlemus93@gmail.com>
2024-04-03 16:50:11 -0700

    added show and index component

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -1,9 +1,31 @@
-import React from "react"
+import React from "react";
+import { Card, CardBody, CardTitle, Button} from "reactstrap"
+import { Link } from "react-router-dom"
 
-const CatIndex = () => {
+const CatIndex = ({ cats }) => {
   return (
-    <h3>CatIndex</h3>
-  )
-}
+    <div className="cat-index-cont">
+      <h1 className="index-title">Meet All the Cats</h1>
+      {cats.map((cat) => {
+        return (
+          <Card
+          key={cat.id}
+            style={{
+              width: "18rem",
+            }}
+          >
+            <img alt={`profile of ${cat.name}`} src={cat.image} />
+            <CardBody>
+              <CardTitle tag="h5">{cat.name}</CardTitle>
+              <Link to={`/cat-show/${cat.id}`}>
+              <Button>See More of Me</Button>
+              </Link>
+            </CardBody>
+          </Card>
+        );
+      })}
+    </div>
+  );
+};
 
-export default CatIndex
+export default CatIndex;
