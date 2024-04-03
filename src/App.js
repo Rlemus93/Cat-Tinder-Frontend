c1b1dae59bb15c45fcf1a9b1b98cfe1bcd22364a
Ryan <rlemus93@gmail.com>
2024-04-03 16:51:54 -0700

    added more styles

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="cat-edit" element={<CatEdit />} />
-        <Route path="cat-index" element={<CatIndex />} />
+        <Route path="cat-index/" element={<CatIndex cats = {cats} />} />
+        <Route path="cat-show/:id" element={<CatShow cats = {cats}/>} />
         <Route path="cat-new" element={<CatNew />} />
-        <Route path="cat-show" element={<CatShow />} />
+        <Route path="cat-edit" element={<CatEdit />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
