ff251b988825a415b128cc37ee0fa6315e3f3c12
smorgannicole <morgann.smithh@yahoo.com>
2024-04-03 13:20:47 -0700

    ✅ home test

---
+++ src/__tests__/Home.test.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import { BrowserRouter } from "react-router-dom"
+import Home from "../pages/Home"
+import heroImage from "../assets/hero-image.png"
+
+test("renders the Home component", () => {
+  render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  )
+  const homeTitle = screen.getByText("CAT TINDER")
+  expect(homeTitle).toBeInTheDocument()
+
+  const homeSubTitle = screen.getByText("Find Your Purrfect Match")
+  expect(homeSubTitle).toBeInTheDocument()
+
+  const homeHeroImage = screen.getByAltText("grey cat with glasses on")
+  expect(homeHeroImage).toBeInTheDocument()
+  expect(homeHeroImage).toHaveAttribute(
+    "src",
+    heroImage
+  )
+})
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from "react"
-import homeHeroImage from "../assets/home-hero-image.png"
+import heroImage from "../assets/hero-image.png"
 
 const Home = () => {
   return (
     <div className="home-cont">
     <h1 className="home-title">CAT <br /> TINDER</h1>
-    <h3 className="home-sub-title"> Find Your Purrfect Match</h3>
-    <img className="home-hero-image" src= { homeHeroImage } alt="grey cat with glasses on" />
+    <h3 className="home-sub-title">Find Your Purrfect Match</h3>
+    <img className="home-hero-image" src= { heroImage } alt="grey cat with glasses on" />
     </div>
   )
 }
