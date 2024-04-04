bcc960fbc5b44951069636b34fbb802dd8a17166
smorgannicole <morgann.smithh@yahoo.com>
2024-04-04 10:34:19 -0700

    💄 redid logo, added accent image, added call to action on hero, a couple animations on home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import { Nav } from "reactstrap"
 import { NavLink } from "react-router-dom"
 import catTinderLogo from "../assets/cat-tinder-logo.png"
+import speechBubbleImage from "../assets/speech-bubble-image.png"
 
 const Header = () => {
+  const [logoIsHovered, setLogoIsHovered] = useState(false)
+
   return (
     <Nav className="nav-cont" navbar>
       <div className="hider"></div>
       <div className="logo-cont">
         <NavLink className="nav-link" to="/">
           <img
-              src={catTinderLogo}
-              alt="cat tinder logo: the profile of a cat enclosed in a circle"
-              className="cat-tinder-logo"
+            src={catTinderLogo}
+            alt="cat tinder logo: an animated grey kitty with yellow sunglasses"
+            className="cat-tinder-logo"
+            onMouseEnter={() => setLogoIsHovered(true)}
+            onMouseLeave={() => setLogoIsHovered(false)}
+          />
+          {logoIsHovered && (
+            <img
+              src={speechBubbleImage}
+              alt='A white speech bubble with black text reading: "Take me home!"'
+              className="speech-bubble-image"
             />
+          )}
         </NavLink>
       </div>
       <div className="navigation-links">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
 import React from "react"
 import heroImage from "../assets/hero-image.png"
+import accentHeroImage from "../assets/accent-hero-image.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons"
 
 const Home = () => {
   return (
     <div className="home-cont">
-    <h1 className="home-title">CAT <br /> TINDER</h1>
-    <h3 className="home-sub-title">Find Your Purrfect Match</h3>
-    <img className="home-hero-image" src= { heroImage } alt="grey cat with glasses on" />
+      <img className="accent-hero-image" src={accentHeroImage} alt="an accent wave faded in the background" />
+      <h1 className="home-title">CAT <br /> TINDER</h1>
+      <h3 className="home-sub-title">Find Your Purrfect Match <FontAwesomeIcon className="home-arrow-icon" icon={faCircleArrowRight} /></h3>
+      <img className="home-hero-image-shadow" src= { heroImage } alt="cat with sunglasses' shadow" />
+      <img className="home-hero-image" src= { heroImage } alt="grey cat with glasses on" />
     </div>
   )
 }
