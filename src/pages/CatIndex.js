a6cec8593ac75c712d96c76ea76ad232d0753fb5
Ryan <rlemus93@gmail.com>
2024-04-04 15:25:53 -0700

    minor styling

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -4,30 +4,33 @@ import { Link } from "react-router-dom";
 
 const CatIndex = ({ cats }) => {
   return (
-    <div className="cat-index-cont">
+    <div className="index-cont">
       <h1 className="index-title">Meet All the Cats</h1>
-      {cats.map((cat) => {
-        return (
-          <Card
-            key={cat.id}
-            style={{
-              width: "18rem",
-            }}
-          >
-            <img
-              className="profile-image"
-              alt={`profile of ${cat.name}`}
-              src={cat.image}
-            />
-            <CardBody>
-              <CardTitle tag="h5">{cat.name}</CardTitle>
-              <Link to={`/cat-show/${cat.id}`}>
-                <Button>See More of Me</Button>
-              </Link>
-            </CardBody>
-          </Card>
-        );
-      })}
+      <div className="cat-index-cards">
+        {cats.map((cat) => {
+          return (
+            <div className="index-card" key={cat.id}>
+              <Card
+                style={{
+                  width: "18rem",
+                }}
+              >
+                <img
+                  className="profile-image"
+                  alt={`profile of ${cat.name}`}
+                  src={cat.image}
+                />
+                <CardBody>
+                  <CardTitle tag="h5">{cat.name}</CardTitle>
+                  <Link to={`/cat-show/${cat.id}`}>
+                    <Button>See More of Me</Button>
+                  </Link>
+                </CardBody>
+              </Card>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/pages/CatShow.js
+++ src/pages/CatShow.js
@@ -1,17 +1,31 @@
-import React from "react"
-import { useParams } from "react-router-dom"
+import React from "react";
+import { useParams } from "react-router-dom";
+import { Button } from "reactstrap"
+import { Link } from "react-router-dom";
 
 const CatShow = ({ cats }) => {
-  const { id } = useParams()
-  const cat = cats.find((catObject) => catObject.id === +id)
+  const { id } = useParams();
+  const cat = cats.find((catObject) => catObject.id === +id);
   return (
-      <div className="">
+    <div className="">
       <h1> Meet {cat.name}</h1>
       <p>Age {cat.age}</p>
-      <p>{cat.name} enjoys {cat.enjoys}</p>
-      <img src={cat.image} alt={`profile of ${cat.name}`} />
-      </div>
-  )
-}
+      <p>
+        {cat.name} enjoys {cat.enjoys}
+      </p>
+      <img
+        className="show-image"
+        src={cat.image}
+        alt={`profile of ${cat.name}`}
+      />
+      <Link to={`/cat-edit/${cat.id}`}>
+        <Button> Edit </Button>
+      </Link>
+      <Link to={`/cat-index`}>
+        <Button>Back</Button>
+      </Link>
+    </div>
+  );
+};
 
-export default CatShow
+export default CatShow;
