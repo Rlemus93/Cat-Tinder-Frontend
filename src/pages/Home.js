1028367e7cbb962df6bab12a47844be1ce18f7a4
Ryan <rlemus93@gmail.com>
2024-04-04 15:05:08 -0700

    syntax updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,42 @@
-import React from "react"
-import heroImage from "../assets/hero-image.png"
-import accentHeroImage from "../assets/accent-hero-image.png"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons"
-import { Link } from "react-router-dom"
+import React from "react";
+import heroImage from "../assets/hero-image.png";
+import accentHeroImage from "../assets/accent-hero-image.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="home-cont">
-      <img className="accent-hero-image" src={accentHeroImage} alt="an accent wave faded in the background" />
-      <h1 className="home-title">CAT <br /> TINDER</h1>
+      <img
+        className="accent-hero-image"
+        src={accentHeroImage}
+        alt="an accent wave faded in the background"
+      />
+      <h1 className="home-title">
+        CAT <br /> TINDER
+      </h1>
       <Link to="/cat-index">
-        <h3 className="home-sub-title">Find Your Purrfect Match <FontAwesomeIcon className="home-arrow-icon" icon={faCircleArrowRight} /></h3>
-        <img className="home-hero-image-shadow" src= { heroImage } alt="cat with sunglasses' shadow" />
+        <h3 className="home-sub-title">
+          Find Your Purrfect Match{" "}
+          <FontAwesomeIcon
+            className="home-arrow-icon"
+            icon={faCircleArrowRight}
+          />
+        </h3>
+        <img
+          className="home-hero-image-shadow"
+          src={heroImage}
+          alt="cat with sunglasses' shadow"
+        />
       </Link>
-      <img className="home-hero-image" src= { heroImage } alt="grey cat with glasses on" />
+      <img
+        className="home-hero-image"
+        src={heroImage}
+        alt="grey cat with glasses on"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
