a1bf8d7db6bf56b7539f3a28309c9843363dd88b
smorgannicole <morgann.smithh@yahoo.com>
2024-04-04 11:37:13 -0700

    📝 added form and test for form

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
-import "./App.css"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import Home from "./pages/Home"
-import CatEdit from "./pages/CatEdit"
-import CatIndex from "./pages/CatIndex"
-import CatNew from "./pages/CatNew"
-import CatShow from "./pages/CatShow"
-import NotFound from "./pages/NotFound"
-import mockCats from "./mockCats"
-import { Routes, Route } from "react-router-dom"
-import React, { useState } from "react"
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import CatEdit from "./pages/CatEdit";
+import CatIndex from "./pages/CatIndex";
+import CatNew from "./pages/CatNew";
+import CatShow from "./pages/CatShow";
+import NotFound from "./pages/NotFound";
+import mockCats from "./mockCats";
+import { Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
 
 const App = () => {
-  const [cats, setCats] = useState(mockCats)
+  const [cats, setCats] = useState(mockCats);
+
+  const createNewCat = (catData) => {
+    console.log(catData);
+  };
+
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="cat-index/" element={<CatIndex cats = {cats} />} />
-        <Route path="cat-show/:id" element={<CatShow cats = {cats}/>} />
-        <Route path="cat-new" element={<CatNew />} />
+        <Route path="cat-index/" element={<CatIndex cats={cats} />} />
+        <Route path="cat-show/:id" element={<CatShow cats={cats} />} />
+        <Route
+          path="cat-new"
+          element={<CatNew createNewCat={createNewCat} />}
+        />
         <Route path="cat-edit" element={<CatEdit />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
